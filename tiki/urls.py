85bed49c9b1f473cf060cb84a1dcc3590ce96171
AbrahamV <abraham.varricatt@googlemail.com>
2016-01-11 03:24:24 +0530

    Displaying article based on id number.
    Refer:  #12

--- tiki/urls.py
+++ tiki/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from tiki import views
 
 urlpatterns = patterns('',
+    url(r'^article/(?P<article_id>\d+)/$', views.article, name='article'),
     url(r'^$', views.index, name='index'),
 )
 
--- tiki/views.py
+++ tiki/views.py
@@ -1,7 +1,32 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from django.template import RequestContext
 from time import strftime
+from tiki.models import Article
+
 
 def index(request):
     current_time = strftime('%Y-%m-%d %H:%M:%S')
     return HttpResponse('Hello World! <br/> Now=' + str(current_time))
 
+
+def article(request, article_id):
+    try:
+        article_model = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404('Article does not exist')
+
+    # Article context
+    context = {
+        'title': article_model.title,
+        'author': article_model.author,
+        'pub_date': article_model.pub_date,
+        'category': article_model.category,
+        'hero_image': article_model.hero_image,
+        'extra_image': article_model.extra_image,
+        'body_text': article_model.body_text,
+    }
+    return render(request,
+                  'article.html',
+                  context)
