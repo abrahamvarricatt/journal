ef2a335504d13283b339011e2cb4fbec534f64fb
AbrahamV <abraham.varricatt@googlemail.com>
2016-01-11 00:56:59 +0530

    list display for admin view
    Fixes #9

--- tiki/admin.py
+++ tiki/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from tiki.models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date', 'category')
+
+admin.site.register(Article, ArticleAdmin)
 
